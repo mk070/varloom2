2fe65afa5e968f0d8bc36a14c70e921e903fb4af
Madhan <madhan.king51@gmail.com>
2024-06-07 15:28:11 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 
 import './App.css'
+import Header from './layout/Header'
 
 function App() {
 
   return (
     <>
+    <Header />
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <h1 className="text-4xl font-bold text-blue-600 ">
         Hello, Tailwind CSS!
       </h1>
-    </div>
+      </div>
     </>
   )
 }
