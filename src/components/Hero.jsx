ede0d483d836609537a606cf820f76eab689c135
Madhan <madhan.king51@gmail.com>
2024-06-09 19:08:00 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,10 @@
-import React,{useEffect} from 'react'
+import React, { useState, useEffect } from 'react';
+
+const colors = [
+  'bg-primary-white',
+  'bg-trinary-green',
+  'bg-neutral-blue',
+];
 
 export const Hero = () => {
   const [currentColorIndex, setCurrentColorIndex] = useState(0);
@@ -12,32 +18,10 @@ export const Hero = () => {
 
     return () => clearInterval(intervalId);
   }, []);
-  useEffect(() =>{
-    const characters = document.querySelectorAll('.char');
-    characters.forEach((char,index) => {
-      setTimeout(() =>{
-        char.classList.add('animated');
-      }, index* 900);
-    });
-  },[]);
 
   return (
-    <>
-        <div className="flex justify-center items-center h-screen">
-          <div className="flex flex-col items-center">
-            <div className="text-6xl flex gap-1">
-              <span>T</span>
-              <span>R</span>
-              <span className='char'>A</span>
-              <span>N</span>
-              <span className='char'>S</span>
-              <span>F</span>
-              <span>O</span>
-              <span className='char'>R</span>
-              <span>M</span>
-            </div>
-          </div>
-        </div>
-    </>  
-)
-}
+    <div className={`hero flex h-screen w-full items-center justify-center ${colors[currentColorIndex]}`}>
+      Hero
+    </div>
+  );
+};
