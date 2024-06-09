95feba19179184e10fd7afb5368037bc54b8549c
naveenrk7 <111naveenkumarrk@gmail.com>
2024-06-10 03:54:06 +0530

    commit hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import textFit from 'textfit';
 
 const colors = [
   'bg-primary-white',
@@ -12,52 +13,46 @@ export const Hero = () => {
   useEffect(() => {
     const changeColor = () => {
       setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
-
     };
 
-
     // text animation
-    const characters = document.querySelectorAll('.char');
-    characters.forEach((char, index) =>{
+    const characters = document.querySelectorAll('.letters');
+    characters.forEach((letters, index) =>{
       setTimeout(() => {
-        char.classList.add('animated');
-      },index *2000);
+        letters.classList.add('animated');
+      },index *1000);
     });
-
 
     const intervalId = setInterval(changeColor, 4000);
     return () => clearInterval(intervalId);
   },[]);
 
+  useEffect(() => {
+    textFit(document.getElementsByClassName('hero-title'));
+    textFit(document.getElementsByClassName('hero-title_2'));
+  }, []); // Run once after the component mounts
 
   return (
     <div className={`hero  flex h-screen flex-col w-full items-center justify-center ${colors[currentColorIndex]}`}>
-      <div className="flex text-[190px] gap-1">
-        <span className='char'>T</span>
-        <span className='txt'>R</span>
-        <span className='char'>A</span>
-        <span className='txt'>N</span>
-        <span className='txt'>S</span>
-        <span className='char'>F</span>
-        <span className='txt'>O</span>
-        <span className='txt'>R</span>
-        <span className='char'>M</span>
-      {/* <div className="flex flex-row gap-24">
-      <div className="flex text-[160px] gap-1">
-        <span className='char'>I</span>
-        <span className='txt'>D</span>
-        <span className='char'>E</span>
-        <span className='txt'>S</span>
-      </div> 
-      <div className="flex text-[160px] gap-1">
-        <span className='char'>H</span>
-        <span className='txt'>E</span>
-        <span className='txt'>R</span>
-        <span className='char'>E</span>
-      </div>  */}
+      <div className="hero-title flex gap-1">
+        <span className='letters'>T</span>
+        <span >R</span>
+        <span className='letters'>A</span>
+        <span >N</span>
+        <span >S</span>
+        <span className='letters'>F</span>
+        <span >O</span>
+        <span >R</span>
+        <span className='letters'>M</span>
+        </div>
+      <div className="hero-title_2 flex text-[80px] gap-1 my-[-100px]">
+        <span className='letters'>M</span>
+        <span >A</span>
+        <span >R</span>
+        <span className='letters'>K</span>
+        <span >E</span>
+        <span className='letters'>T</span>
       </div> 
     </div>
   );
 };
-
-
