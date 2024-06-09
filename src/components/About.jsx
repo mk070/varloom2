45fec9907962e965c8a36c27a7b78903d6f15783
naveenrk7 <111naveenkumarrk@gmail.com>
2024-06-09 23:10:55 +0530

    commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,6 +11,6 @@ export const About = () => {
   return (
     <div className='flex h-screen w-full items-start bg-secondary-black justify-center'>
         <h1 className='text-primary-white'>Welcome to [Your Agency Name], where creativity meets cutting-edge technology. We are a premier creative agency specializing in web development and design, with a distinct emphasis on 3D animations and interactive, engaging experiences. Our passion is to bring your vision to life with futuristic designs that make your website stand out in the digital landscape.</h1>
-    </div>
+    </div>  
   )
 }
