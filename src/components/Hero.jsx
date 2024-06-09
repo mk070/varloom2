5a81c00662b62267075de5cb2e3a3d8ed10482cc
naveenrk7 <111naveenkumarrk@gmail.com>
2024-06-09 15:12:25 +0530

    COMMIT

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const Hero = () => {
   return (
     <>
-        <div className="hero flex h-screen w-full items-center bor justify-center ">
-            Hero
+        <div className="hero flex h-screen w-full items-center bor  justify-center text-[100px] text-center">
+           hero<br/> section
         </div>
     </>  
 )
