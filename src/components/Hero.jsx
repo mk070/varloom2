e83c6cb518dfda916fb0402b5a21fd6bf88b9773
naveenrk7 <111naveenkumarrk@gmail.com>
2024-06-09 19:01:36 +0530

    commiot

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 export const Hero = () => {
+  useEffect(() =>{
+    const characters = document.querySelectorAll('.char');
+    characters.forEach((char,index) => {
+      setTimeout(() =>{
+        char.classList.add('animated');
+      }, index* 900);
+    });
+  },[]);
+
   return (
     <>
-        <div className="hero flex h-screen w-full items-center bor  justify-center text-[100px] text-center">
-           hero<br/> section
+        <div className="flex justify-center items-center h-screen">
+          <div className="flex flex-col items-center">
+            <div className="text-6xl flex gap-1">
+              <span>T</span>
+              <span>R</span>
+              <span className='char'>A</span>
+              <span>N</span>
+              <span className='char'>S</span>
+              <span>F</span>
+              <span>O</span>
+              <span className='char'>R</span>
+              <span>M</span>
+            </div>
+          </div>
         </div>
     </>  
 )
