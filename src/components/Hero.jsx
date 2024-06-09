1b42b499e9c123dba90fe67ba904b5c2498bf8c6
Madhan <madhan.king51@gmail.com>
2024-06-09 16:55:21 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
+const colors = [
+  'bg-primary-white',
+  'bg-trinary-green',
+  'bg-neutral-blue',
+];
 
 export const Hero = () => {
+  const [currentColorIndex, setCurrentColorIndex] = useState(0);
+
+  useEffect(() => {
+    const changeColor = () => {
+      setCurrentColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    };
+
+    const intervalId = setInterval(changeColor, 2000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <>
-        <div className="hero flex h-screen w-full items-center bor justify-center ">
-            Hero
-        </div>
-    </>  
-)
-}
+    <div className={`hero flex h-screen w-full items-center justify-center ${colors[currentColorIndex]}`}>
+      Hero
+    </div>
+  );
+};
--- src/layout/Logo.jsx
+++ src/layout/Logo.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 const Logo = () => {
   return (
-    <div className="logo-wrapper">
+    <div className="logo-wrapper mt-3">
       <div className="logo-container">
-        <span className="initial-text">{'{VL}'}</span>
-        <span className="full-text">{'{Varloom}'}</span>
+        <span className="initial-text font-blink">
+          <span className='text-trinary-green'>{'{'}</span>
+            VL
+          <span className='text-neutral-blue'>{'}'}</span>
+        </span>
+        <span className="full-text font-blink">
+          <span className='text-trinary-green'>{'{'}</span>
+           Varloom
+          <span className='text-neutral-blue'>{'}'}</span>
+        </span>
       </div>
     </div>
   );
